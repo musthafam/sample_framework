1a332b161947e16679f154bafde5182520d4f6e3
musthafam <mmmgce@gmail.com>
2018-09-22 16:10:33 +0530

    parameters update

--- src/main/java/com/practice/framework/utils/DriverBase.java
+++ src/main/java/com/practice/framework/utils/DriverBase.java
@@ -13,6 +13,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
@@ -38,13 +40,15 @@ public class DriverBase {
 		extent.loadConfig(new File(ExtentConfigPath));
 	}
 	
+	@Parameters("browserName")
 	@BeforeMethod
-	public void startDriver(Method method) {
+	public void startDriver(@Optional("chrome") String browserName, Method method) {
 		test = extent.startTest(this.getClass().getSimpleName()+"::"+method.getName());
-		
-		test.log(LogStatus.PASS, "Browser Launched Successfully");
-		
-		
+		if(!browser.isEmpty()) {
+			test.log(LogStatus.INFO, "Launching "+ browser +" Browser");
+		} else {
+			test.log(LogStatus.INFO, "Launching "+browserName+" Browser");
+		}
 	}
 
 	@AfterMethod
@@ -53,27 +57,51 @@ public class DriverBase {
 		extent.endTest(test);
 	}
 	
+	@Parameters("browserName")
 	@BeforeTest
-	public void startDriverBeforeTest(){
-		if(browser.equalsIgnoreCase("chrome")) {
-			String chromeExePath = System.getProperty("user.dir")+File.separator+
-					"src"+File.separator+"test"+File.separator+"resources"+File.separator+
-					"drivers"+File.separator+"chromedriver.exe";
-			System.setProperty("webdriver.chrome.driver", chromeExePath);
-			driver = new ChromeDriver();
-		} else if(browser.equalsIgnoreCase("firefox")) {
-			String geckoExePath = System.getProperty("user.dir")+File.separator+
-					"src"+File.separator+"test"+File.separator+"resources"+File.separator+
-					"drivers"+File.separator+"geckodriver.exe";
-			System.setProperty("webdriver.gecko.driver", geckoExePath);
-			driver = new FirefoxDriver();
-		} else if(browser.equalsIgnoreCase("ie")) {
-			String geckoExePath = System.getProperty("user.dir")+File.separator+
-					"src"+File.separator+"test"+File.separator+"resources"+File.separator+
-					"drivers"+File.separator+"IEDriverServer.exe";
-			System.setProperty("webdriver.ie.driver", geckoExePath);
-			driver = new InternetExplorerDriver();
+	public void startDriverBeforeTest(@Optional("chrome") String browserName){
+		if(!browser.isEmpty()) {
+			if(browser.equalsIgnoreCase("chrome")) {
+				String chromeExePath = System.getProperty("user.dir")+File.separator+
+						"src"+File.separator+"test"+File.separator+"resources"+File.separator+
+						"drivers"+File.separator+"chromedriver.exe";
+				System.setProperty("webdriver.chrome.driver", chromeExePath);
+				driver = new ChromeDriver();
+			} else if(browser.equalsIgnoreCase("firefox")) {
+				String geckoExePath = System.getProperty("user.dir")+File.separator+
+						"src"+File.separator+"test"+File.separator+"resources"+File.separator+
+						"drivers"+File.separator+"geckodriver.exe";
+				System.setProperty("webdriver.gecko.driver", geckoExePath);
+				driver = new FirefoxDriver();
+			} else if(browser.equalsIgnoreCase("ie")) {
+				String geckoExePath = System.getProperty("user.dir")+File.separator+
+						"src"+File.separator+"test"+File.separator+"resources"+File.separator+
+						"drivers"+File.separator+"IEDriverServer.exe";
+				System.setProperty("webdriver.ie.driver", geckoExePath);
+				driver = new InternetExplorerDriver();
+			} 
+		} else {
+			if(browserName.equalsIgnoreCase("chrome")) {
+				String chromeExePath = System.getProperty("user.dir")+File.separator+
+						"src"+File.separator+"test"+File.separator+"resources"+File.separator+
+						"drivers"+File.separator+"chromedriver.exe";
+				System.setProperty("webdriver.chrome.driver", chromeExePath);
+				driver = new ChromeDriver();
+			} else if(browserName.equalsIgnoreCase("firefox")) {
+				String geckoExePath = System.getProperty("user.dir")+File.separator+
+						"src"+File.separator+"test"+File.separator+"resources"+File.separator+
+						"drivers"+File.separator+"geckodriver.exe";
+				System.setProperty("webdriver.gecko.driver", geckoExePath);
+				driver = new FirefoxDriver();
+			} else if(browserName.equalsIgnoreCase("ie")) {
+				String geckoExePath = System.getProperty("user.dir")+File.separator+
+						"src"+File.separator+"test"+File.separator+"resources"+File.separator+
+						"drivers"+File.separator+"IEDriverServer.exe";
+				System.setProperty("webdriver.ie.driver", geckoExePath);
+				driver = new InternetExplorerDriver();
+			}
 		}
+		
 	}
 	
 	@AfterTest
